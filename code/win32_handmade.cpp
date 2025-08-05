9401ba37fb153aaa9498e4fe9f03a0f8fc84744d
d.lulakov <d.lulakov@gmail.com>
2025-08-05 23:22:57 +0200

    Added update to the handmadehero need to continue

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,48 +1,84 @@
+#include <stdint.h>
 #include <windows.h>
 
 #define internal static
 #define local_persist static
 #define global_variable static
 
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
 // TODO: This is a global for now.
 global_variable bool Running;
 global_variable BITMAPINFO BitMapInfo;
 global_variable void *BitmapMemory;
-global_variable HBITMAP BitmapHandle;
-global_variable HDC BitMapDeviceContext;
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
 
 internal void Win32ResizeDIBSection(int Width, int Height) {
   // TODO: Bulletproof this, maybe don't free first, free after, then free first
   // if that fails;
 
-  // TODO: Free our DIBSection
-  //
-
-  if (BitmapHandle) {
-    DeleteObject(BitmapHandle);
-  }
-  if (!BitMapDeviceContext) {
-
-    // TODO: should we recrate these udner cretain special circumstances
-    BitMapDeviceContext = CreateCompatibleDC(0);
+  if (BitmapMemory) {
+    VirtualFree(BitmapMemory, 0, MEM_RELEASE);
   }
 
-  BITMAPINFO BitMapInfo;
+  BitmapWidth = Width;
+  BitmapHeight = Height;
+
   BitMapInfo.bmiHeader.biSize = sizeof(BitMapInfo.bmiHeader);
-  BitMapInfo.bmiHeader.biWidth = Width;
-  BitMapInfo.bmiHeader.biHeight = Height;
+  BitMapInfo.bmiHeader.biWidth = BitmapWidth;
+  BitMapInfo.bmiHeader.biHeight = -BitmapHeight;
   BitMapInfo.bmiHeader.biPlanes = 1;
   BitMapInfo.bmiHeader.biBitCount = 32;
   BitMapInfo.bmiHeader.biCompression = BI_RGB;
 
-  BitmapHandle = CreateDIBSection(BitMapDeviceContext, &BitMapInfo,
-                                  DIB_RGB_COLORS, &BitmapMemory, 0, 0);
+  int BytesPerPixel = 4;
+  int BitmapMemorySize = BytesPerPixel * (BitmapWidth * BitmapHeight);
+  BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
+  int Pitch = Width * BytesPerPixel;
+  uint8 *Row = (uint8 *)BitmapMemory;
+  for (int Y = 0; Y < BitmapHeight; ++Y) {
+    uint8 *Pixel = (uint8 *)Row;
+    for (int X = 0; X < BitmapWidth; ++X) {
+      /*
+       Pixel in memory: BB GG RR xx
+       */
+      *Pixel = 0;
+      ++Pixel;
+
+      *Pixel = 0;
+      ++Pixel;
+
+      *Pixel = 255;
+      ++Pixel;
+
+      *Pixel = 0;
+      ++Pixel;
+    }
+    Row += Pitch;
+  }
 }
 
-internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width,
-                                int Height) {
-  StretchDIBits(DeviceContext, X, Y, Width, Height, X, Y, Width, Height,
-                BitmapMemory, &BitMapInfo, DIB_RGB_COLORS, SRCCOPY);
+internal void Win32UpdateWindow(HDC DeviceContext, RECT *WindowRect, int X,
+                                int Y, int Width, int Height) {
+
+  int WindowWidth = WindowRect->right - WindowRect->left;
+  int WindowHeight = WindowRect->bottom - WindowRect->top;
+  StretchDIBits(DeviceContext,
+                /*
+                X, Y, Width, Height, X, Y, Width, Height,*/
+                0, 0, BitmapWidth, BitmapHeight, 0, 0, WindowWidth,
+                WindowHeight, BitmapMemory, &BitMapInfo, DIB_RGB_COLORS,
+                SRCCOPY);
 }
 
 LRESULT CALLBACK Win32MainWindowCallBack(HWND Window, UINT Message,
@@ -71,13 +107,15 @@ LRESULT CALLBACK Win32MainWindowCallBack(HWND Window, UINT Message,
     OutputDebugStringA("WM_ACTIVEAPP\n");
   } break;
   case WM_PAINT: {
+    OutputDebugStringA("WM_PAINT\n");
     PAINTSTRUCT Paint;
     HDC DeviceContext = BeginPaint(Window, &Paint);
     int X = Paint.rcPaint.left;
     int Y = Paint.rcPaint.top;
     int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
     int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-    Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+    RECT ClientRect;
+    Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
     EndPaint(Window, &Paint);
   } break;
   default: {
